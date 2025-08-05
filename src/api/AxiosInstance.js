ed9bb863890f80e5d4615b18e01cef96c3875a89
SurajWebM <surajbuss08@gmail.com>
2025-08-05 11:30:34 +0530

    Axios updte

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,4 +1,34 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const isDevelopment = import.meta.env.MODE === 'development';
+// const myBaseUrl = isDevelopment
+//     ? import.meta.env.VITE_API_BASE_URL_LOCAL
+//     : import.meta.env.VITE_API_BASE_URL_DEPLOY;
+
+// const AxiosInstance = axios.create({
+//     baseURL: myBaseUrl,
+//     timeout: 5000,
+//     headers: {
+//         'Content-Type': 'application/json',
+//         accept: 'application/json',
+//     },
+// });
+
+// // ✅ Add token dynamically to every request
+// AxiosInstance.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem('accessToken');
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     (error) => Promise.reject(error)
+// );
+
+// export default AxiosInstance;
+
+import axios from 'axios'
 
 const isDevelopment = import.meta.env.MODE === 'development';
 const myBaseUrl = isDevelopment
@@ -9,21 +39,9 @@ const AxiosInstance = axios.create({
     baseURL: myBaseUrl,
     timeout: 5000,
     headers: {
-        'Content-Type': 'application/json',
-        accept: 'application/json',
-    },
+        "Content-Type": "application/json",
+        accept: "application/json"
+    }
 });
-
-// ✅ Add token dynamically to every request
-AxiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('accessToken');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
 
 export default AxiosInstance;
